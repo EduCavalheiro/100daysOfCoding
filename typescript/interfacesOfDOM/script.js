46439f22c4be4185bfa112cbb839f3e18a4850be
ecavalheiro <effcavalheiro@gmail.com>
2023-09-16 12:25:01 +0100

    wip(day4): Working with interfaces of DOM

--- typescript/interfacesOfDOM/script.js
+++ typescript/interfacesOfDOM/script.js
@@ -1,2 +1,24 @@
 "use strict";
-const video = document.querySelector("video");
+const links = document.querySelectorAll(".link");
+links.forEach((link) => {
+    console.log(link);
+});
+links.forEach((link) => {
+    if (link instanceof HTMLAnchorElement) {
+        console.log(link.href);
+    }
+});
+const dados = [1, 2, 3];
+const arrayLinks = Array.from(links);
+const anchorLinks = arrayLinks.filter((link) => link instanceof HTMLAnchorElement);
+// ===================================================
+const colorLinks = document.querySelectorAll(".link");
+function activeElement(elemento) {
+    elemento.style.color = "red";
+    elemento.style.border = "2px solid red";
+}
+links.forEach((elemento) => {
+    if (elemento instanceof HTMLElement) {
+        activeElement(elemento);
+    }
+});
--- typescript/interfacesOfDOM/script.ts
+++ typescript/interfacesOfDOM/script.ts
@@ -1,1 +1,32 @@
-const video = document.querySelector("video");
+const links = document.querySelectorAll(".link");
+
+links.forEach((link) => {
+  console.log(link);
+});
+
+links.forEach((link) => {
+  if (link instanceof HTMLAnchorElement) {
+    console.log(link.href);
+  }
+});
+
+const dados = [1, 2, 3];
+
+const arrayLinks = Array.from(links);
+const anchorLinks = arrayLinks.filter(
+  (link) => link instanceof HTMLAnchorElement
+);
+
+// ===================================================
+const colorLinks = document.querySelectorAll(".link");
+
+function activeElement(elemento: HTMLElement) {
+  elemento.style.color = "red";
+  elemento.style.border = "2px solid red";
+}
+
+links.forEach((elemento) => {
+  if (elemento instanceof HTMLElement) {
+    activeElement(elemento);
+  }
+});
