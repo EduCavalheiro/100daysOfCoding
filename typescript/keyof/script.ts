091a7e672ec34a465aa9a4aa9c130e6499d3bab9
ecavalheiro <effcavalheiro@gmail.com>
2023-09-25 23:56:03 +0100

    wip(day11): Some progress

--- typescript/keyof/script.ts
+++ typescript/keyof/script.ts
@@ -1,1 +1,21 @@
-console.log("Template");
+interface Produto {
+  nome: string;
+  preco: number;
+  novo: boolean;
+}
+
+let chave: keyof Produto;
+chave = "nome";
+
+function coordenadas(x: number, y: number) {
+  return { x, y };
+}
+
+let coord: typeof coordenadas;
+
+coord = (x: number, y: number) => {
+  return {
+    x,
+    y,
+  };
+};
